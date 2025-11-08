09163451d3e28b5e1ccf1dcb6afbc029ddc32ea4
Nicolas Pinzon <nicolas.pinzon@aossas.com>
2025-11-08 22:24:20 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "AUTH_DOMAIN",
-  projectId: "PROJECT_ID",
-  storageBucket: "STORAGE_BUCKET",
-  messagingSenderId: "MESSAGING_SENDER_ID",
-  appId: "APP_ID",
+  apiKey: "AIzaSyArMhE9-gJLxoJEE5Vsf2qRFb9CGft3AD0",
+  authDomain: "studio-9337256305-1d9c5.firebaseapp.com",
+  projectId: "studio-9337256305-1d9c5",
+  storageBucket: "studio-9337256305-1d9c5.appspot.com",
+  messagingSenderId: "646702386398",
+  appId: "1:646702386398:web:9ce47e984f4b269725464f",
 };
