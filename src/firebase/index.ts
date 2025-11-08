19d60809e5a7f5d8c3ba4fffdc43809af7d7b8ef
Nicolas Pinzon <nicolas.pinzon@aossas.com>
2025-11-08 22:30:53 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -30,7 +30,6 @@ export * from './provider';
 export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
-export * from './auth/use-user';
 export * from './errors';
 export * from './error-emitter';
 export * from './non-blocking-updates';
