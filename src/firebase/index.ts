149719285dbf19ff9e4502ee9e80103170e6bc5c
Nicolas Pinzon <nicolas.pinzon@aossas.com>
2025-11-09 00:02:42 +0000

    Set up a Firebase backend

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -7,20 +7,29 @@ import { getFirestore } from 'firebase/firestore'
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
-  if (getApps().length > 0) {
-    return getSdks(getApp());
+  if (!getApps().length) {
+    // Important! initializeApp() is called without any arguments because Firebase App Hosting
+    // integrates with the initializeApp() function to provide the environment variables needed to
+    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
+    // without arguments.
+    let firebaseApp;
+    try {
+      // Attempt to initialize via Firebase App Hosting environment variables
+      firebaseApp = initializeApp();
+    } catch (e) {
+      // Only warn in production because it's normal to use the firebaseConfig to initialize
+      // during development
+      if (process.env.NODE_ENV === "production") {
+        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
+      }
+      firebaseApp = initializeApp(firebaseConfig);
+    }
+
+    return getSdks(firebaseApp);
   }
 
-  // In a production environment (deployed on Firebase App Hosting),
-  // initializeApp() can be called without arguments to use the auto-configured
-  // environment variables. In all other cases (like local development),
-  // we must pass the explicit firebaseConfig object.
-  const isProduction = process.env.NODE_ENV === 'production';
-  const firebaseApp = isProduction
-    ? initializeApp()
-    : initializeApp(firebaseConfig);
-
-  return getSdks(firebaseApp);
+  // If already initialized, return the SDKs with the already initialized App
+  return getSdks(getApp());
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
@@ -39,4 +48,3 @@ export * from './non-blocking-updates';
 export * from './non-blocking-login';
 export * from './errors';
 export * from './error-emitter';
-export * from './auth/use-user';
